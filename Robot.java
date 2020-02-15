77291f2736bebd8ffc5688e2c4df6059b6008704
michaelallen1994 <michael.allen@kckps.org>
2020-02-15 14:21:50 -0600

    Example Program
    Trying to use this to test our wiring, connectivity, and joysticks.

---
+++ Robot.java
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+//import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * This is a demo program showing the use of the RobotDrive class, specifically
+ * it contains the code necessary to operate a robot with tank drive.
+ */
+public class Robot extends TimedRobot {
+  private DifferentialDrive m_myRobot;
+  private Joystick joystick;
+
+  @Override
+  public void robotInit() {
+    m_myRobot = new DifferentialDrive(new Spark(0), new Spark(1));
+    joystick = new Joystick(0);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    m_myRobot.tankDrive(joystick.getY(), joystick.getZ());
+  }
+}
